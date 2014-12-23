22025cf1d1d8dccc0067b47c2cf819eae45f4c25
Yuan Hsiang Cheng <lyforever62@hotmail.com>
2014-12-23 20:35:52 +0800

    convert channel, member id to name

--- startups.js
+++ startups.js
@@ -1,12 +1,12 @@
 var slackbot = require('./lib/bot');
 var http = require('http');
 var querystring = require('querystring');
-
+var request = require('request');
 var config = {
     server: 'irc.freenode.com',
     nick: '_slack_bot',
     username: 'mslackbot612',
-    token: '', //no use
+    token: '', // get from https://api.slack.com/web#basics
     income_url: '',
     outcome_token: '',
     channels: {
@@ -18,6 +18,26 @@ var config = {
     floodProtection: true,
     silent: false // keep the bot quiet
 };
+var slackUsers = {};
+var slackChannels = {};
+
+request.get({
+    url: 'https://slack.com/api/users.list?token=' + config.token
+}, function (error, response, body) {
+  var res = JSON.parse(body);
+  res.members.map(function (member) {
+    slackUsers[member.id] = member.name;
+  });
+});
+
+request.get({
+    url: 'https://slack.com/api/channels.list?token=' + config.token
+}, function (error, response, body) {
+  var res = JSON.parse(body);
+  res.channels.map(function (channel) {
+    slackChannels[channel.id] = channel.name;
+  });
+});
 
 var slackbot = new slackbot.Bot(config);
 slackbot.listen();
@@ -26,11 +46,17 @@ var server = http.createServer(function (req, res) {
   if (req.method == 'POST') {
     req.on('data', function(data) {
       var payload = querystring.parse(data.toString());
-
       if (payload.token == config.outcome_token && payload.user_name != 'slackbot') {
         var ircMsg = "<" + payload.user_name + "> " + payload.text;
         var channel = Object.keys(config.channels)[0];
-
+        ircMsg = ircMsg.replace(/<#C\w{8}>/g, function (matched) {
+            var channel_id = matched.match(/#(C\w{8})/)[1];
+            return '#' + slackChannels[channel_id];
+        });
+        ircMsg = ircMsg.replace(/<@U\w{8}>/g, function (matched) {
+            var member_id = matched.match(/@(U\w{8})/)[1];
+            return '@' + slackUsers[member_id];
+        });
         slackbot.speak(channel, ircMsg);
         res.end('done');
       }
